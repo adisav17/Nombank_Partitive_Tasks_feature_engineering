16564382b74f2427172e80320e4adfb17ba708dc
adisav17 <adisav17@gmail.com>
2023-10-23 14:14:38 +0530

    Added code and data

---
+++ nombank_partitives_proj/build_features.py
@@ -0,0 +1,7 @@
+import create_embedding_and_features
+create_embedding_and_features.make_percent_training_feature_file()
+create_embedding_and_features.make_percent_dev_feature_file()
+create_embedding_and_features.make_percent_test_feature_file()
+create_embedding_and_features.make_part_training_feature_file()
+create_embedding_and_features.make_part_dev_feature_file()
+create_embedding_and_features.make_part_test_feature_file()
